495ca9223579dd807009ba25eac5330e6e2d8ae7
Jason Li <jasonli2446@gmail.com>
2024-12-29 15:47:14 -0500

    added to cart message

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -41,7 +41,7 @@ products.forEach((product) => {
 
       <div class="product-spacer"></div>
 
-      <div class="added-to-cart">
+      <div class="added-to-cart js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
       </div>
@@ -90,5 +90,9 @@ document.querySelectorAll('.js-add-to-cart')
 
       document.querySelector('.js-cart-quantity')
         .innerHTML = cartQuantity;
+
+      const addedMessage = document.querySelector(`.js-added-to-cart${productId}`);
+
+      addedMessage.classList.add('added-to-cart-visible');
     });
   });
